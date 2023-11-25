0bac9167d65a8bf8f3a4cbc7459bfa007f4b9411
Tharindu Bandara <tharinduudeni@gmail.com>
2023-11-25 12:29:22 +0000

    booking feature implementation

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,27 +1,65 @@
-function BookingForm() {
+const BookingForm = (props) => {
+  const {
+    date,
+    setDate,
+    availableTimes,
+    dispatchAvailableTimes,
+    setTime,
+    time,
+    numOfGuests,
+    setNumOfGuests,
+    occasions,
+    setOccation,
+    occasion,
+  } = props;
+
+  const handleAvailableTimes = (selectedDate) => {
+    setDate(selectedDate);
+
+    dispatchAvailableTimes(selectedDate);
+  };
+
   return (
     <form className="booking-form">
-      <label for="res-date">Choose date</label>
-      <input type="date" id="res-date" />
-      <label for="res-time">Choose time</label>
-      <select id="res-time ">
-        <option>17:00</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-        <option>21:00</option>
-        <option>22:00</option>
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        value={date}
+        onChange={(e) => handleAvailableTimes(e.target.value)}
+      />
+      <label htmlFor="res-time">Choose time</label>
+      <select
+        id="res-time "
+        onChange={(e) => setTime(e.target.value)}
+        value={time}
+      >
+        {availableTimes &&
+          availableTimes.map((item) => <option key={item}>{item}</option>)}
       </select>
-      <label for="guests">Number of guests</label>
-      <input type="number" placeholder="1" min="1" max="10" id="guests" />
-      <label for="occasion">Occasion</label>
-      <select id="occasion">
-        <option>Birthday</option>
-        <option>Anniversary</option>
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        value={numOfGuests}
+        onChange={(e) => setNumOfGuests(e.target.value)}
+      />
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        onChange={(e) => setOccation(e.target.value)}
+        value={occasion}
+      >
+        {occasions.map((item) => (
+          <option key={item}>{item}</option>
+        ))}
       </select>
       <input type="submit" value="Make Your reservation" />
     </form>
   );
-}
+};
 
 export default BookingForm;
